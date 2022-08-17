e48178669c0a5f7b2cbb0a4da09adf1dde5803e3
fsuh <tofuhsuh@gmail.com>
2022-08-17 14:59:48 +0300

    added loop task

---
+++ loops_task_1/main.js
@@ -0,0 +1,14 @@
+for (let odds = 1; odds <= 100; odds = odds ++) {
+    if (odds % 3 > 0);
+    console.log(odds);
+}
+
+for (let evens = 1; evens <= 100; evens = evens ++) {
+    if (evens % 2 == 0);
+    console.log(evens);
+}
+
+function speeds(){
+    let dist = Number(prompt("What is the distance?"));
+    let times = Number(prompt(""))
+}
