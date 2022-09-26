6eee9ea9b999985396b999d225b7b8beb669afd8
fsuh <92304761+fsuh@users.noreply.github.com>
2022-09-27 02:06:40 +0300

    adjustment on js speedgame

--- Speedgame/main.js
+++ Speedgame/main.js
@@ -1,35 +1,38 @@
 const startButton = document.querySelector('#start');
-const endButton = document.querySelector('#start');
+const endButton = document.querySelector('#end');
 const circles = document.querySelectorAll('.circle');
-const score = document.querySelector('#display')
-const finalScore = document.querySelector('#final_display')
+const score = document.querySelector('#display');
+const finalScore = document.querySelector('#final_display');
+const closeButton = document.querySelector('.btn_close');
+const overlay = document.querySelector('.overlay');
 let count = 0;
 let active = 0;
 let timer;
 let pace = 1000;
 let rounds = 0;
 
+// function to generate a non-repeating random number
 const getRndInt = (min, max) => {
-    return Math.floor(Math.random()* (max -min +1)) + min; 
+    return Math.floor(Math.random() * (max - min + 1)) + min; 
 }
 
 circles.forEach((circle, i) => {
-    circle.addEventListener('click', () => clickedCircle(i));
+    circle.addEventListener('click', () => rndomCircle(i));
     
 });
 
-const clickedCircle = (i) => {
+// Generating a function of random active circles
+
+const rndomCircle = (i) => {
     if(i !== active) {
         endGame();
     } else {
         count++;
         rounds--;
         score.textContent = count;
-        /*finalScore.textContent = score;*/
+        finalScore.textContent = count;
     }
-    /*console.log('circle was clicked', i + 1);
-    count++;*/
-    /*score.textContent = count;*/
+   
 }
 
 
@@ -54,18 +57,23 @@ const startGame = () => {
 
     function pickNew(active) {
         let nextActive = getRndInt(0,3);
+
         if (nextActive != active) {
             return nextActive
         } else {
             return pickNew(active);
         }
     }
-    
+ startButton.style.display = 'none'
+ endButton.style.display = 'block'
     
 }
 const endGame = () => {
-    /*overlay.style.visibility ='visible';*/
+    overlay.style.visibility ='visible';
+    startButton.style.display = 'block';
+    endButton.style.display = 'none';
     clearTimeout(timer);
+    
 };
 
 const resetGame = () => {
@@ -74,4 +82,4 @@ const resetGame = () => {
 
 startButton.addEventListener('click', startGame)
 endButton.addEventListener('click', endGame)
-/*closeButtoon.addEventListener('click', resetGame)*/
+closeButton.addEventListener('click', resetGame)
