3868ab4af9309688eb40ce76d6b981b11958d983
fsuh <92304761+fsuh@users.noreply.github.com>
2022-10-23 00:01:54 +0300

    completed task Pokedex

--- Pokedex/main.js
+++ Pokedex/main.js
@@ -3,7 +3,7 @@ const genraTion = document.querySelectorAll('.gen');
 const pokeContainer = document.querySelector('#poke_container');
 const searchInput = document.querySelector('.searchPoke')
 const baseURL = 'https://pokeapi.co/api/v2/pokemon/';
-const pokeNum = 10;
+const pokeNum = 500;
 
 let pokemons = [];
 
@@ -14,7 +14,6 @@ const fetchPokemons = async () => {
         }
     } catch(error) {
         console.error(error)
-
     }
     pokemons.forEach((pokemon) => createCard(pokemon));
 };
@@ -37,15 +36,60 @@ const getPokemon = async (id) => {
 
 };
 
+const getGen1 = () => {
+    const searchGen1 = pokemons.filter((pokemon) => pokemon.id <=151);
+        removePokemon()
+        searchGen1.forEach((pokemon) =>createCard(pokemon))
+    }
+const getGen2 = () => {
+    const searchGen2 = pokemons.filter((pokemon) => pokemon.id >=100 && pokemon.id <=151)
+        removePokemon()
+        searchGen2.forEach((pokemon) =>createCard(pokemon))
+    }
+const getGen3 = () => {
+    const searchGen3 = pokemons.filter((pokemon) => pokemon.id >=135 && pokemon.id <= 251);
+        removePokemon()
+        searchGen3.forEach((pokemon) =>createCard(pokemon))
+    }
+const getGen4 = () => {
+    const searchGen4 = pokemons.filter((pokemon) => pokemon.id >=107 && pokemon.id <=387);
+        removePokemon()
+        searchGen4.forEach((pokemon) =>createCard(pokemon))
+    }
+const getGen5 = () => {
+    const searchGen5 = pokemons.filter((pokemon) => pokemon.id >=156 && pokemon.id <=493);
+        removePokemon()
+        searchGen5.forEach((pokemon) =>createCard(pokemon))
+    }
+const getGen6 = () => {
+    const searchGen6 = pokemons.filter((pokemon) => pokemon.id >=72 && pokemon.id <= 649);
+        removePokemon()
+        searchGen6.forEach((pokemon) =>createCard(pokemon))
+    }
+const getGen7 = () => {
+    const searchGen7 = pokemons.filter((pokemon) => pokemon.id >=88 && pokemon.id <= 649);
+        removePokemon()
+        searchGen7.forEach((pokemon) =>createCard(pokemon))
+    }
+const getGen8 = () => {
+    const searchGen8 = pokemons.filter((pokemon) => pokemon.id >=96 && pokemon.id <= 809);
+        removePokemon()
+        searchGen8.forEach((pokemon) =>createCard(pokemon))
+    }
+
+
 const getAllPokemon = async (id) => {
     try{
         const response = await fetch(`${baseURL}${id}`);
         const pokemon = await response.json();
         pokemons = [...pokemons, pokemon];
+        //localStorage.setItem('key', JSON.stringify(pokemons))
+
     } catch(err){
         console.error(err)
     }
 };
+
 fetchPokemons();
 
 const createCard = (pokemon) => {
@@ -80,13 +124,24 @@ form.addEventListener('submit', e => {
 
 });
 
+//const pokeLocal = JSON.parse(localStorage.getItem('key'))
 
-const getGen = () => {
-    const searchGen = pokemons.filter((pokemon) =>{
-        if(pokemon.name >= 151){
-            removePokemon();
-            searchGen.forEach((pokemon) =>createCard(pokemon))
-        }
-    })
-
-}
+genraTion.forEach((item) =>{
+    if(item.textContent === 'Gen1' ) {
+        item.addEventListener('click', getGen1);
+    }else if (item.textContent === 'Gen2'){
+        item.addEventListener('click', getGen2)
+    }else if (item.textContent === 'Gen3'){
+        item.addEventListener('click', getGen3)
+    }else if (item.textContent === 'Gen4'){
+        item.addEventListener('click', getGen4)
+    }else if (item.textContent === 'Gen5'){
+        item.addEventListener('click', getGen5)
+    }else if (item.textContent === 'Gen6'){
+        item.addEventListener('click', getGen6)
+    }else if (item.textContent === 'Gen7'){
+        item.addEventListener('click', getGen7)
+    }else if (item.textContent === 'Gen8'){
+        item.addEventListener('click', getGen7)
+    }
+})
